5b905362514bd08cc6d7c4f2e5c733e23693e90c
ThomasGonet <gonet.thomas@gmail.com>
2014-01-30 17:42:23 +0100

    Ex2

--- L3_TP3/src/ex2/ListeTriée.java
+++ L3_TP3/src/ex2/ListeTriée.java
@@ -1,6 +1,11 @@
 package ex2;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Scanner;
+import java.util.Set;
+import java.util.TreeSet;
 
 public class ListeTriée {
 
@@ -13,19 +18,41 @@ public class ListeTriée {
 		int methode = sc.nextInt();
 		System.out.println("Entrer le nombe d'élément que vous voulez classer:");
 		int nbElement = sc.nextInt();
-		
+		if(methode ==1)
+			methode1(nbElement);
+		else
+			methode2(nbElement);
+
 	}
-	
-	public void methode1()
+
+	public static void methode1(int nb)
 	{
-		
+		//System.out.println("nbel "+ nb);
+		List<Integer> list = new ArrayList<Integer>(nb);
+		for(int i = 0; i < nb; i++)
+		{
+			list.add((int) (Math.random() * 1000));
+		}
+		Collections.sort(list);
+		System.out.println("Méthode 1 :");
+		for(int i: list)
+		{
+			System.out.println(i);
+		}
 	}
-	
-	public void methode2()
+
+	public static void methode2(int nb)
 	{
-		
+		Set<Integer> set = new TreeSet<Integer>();
+		for(int i = 0; i < nb; i++)
+		{
+			set.add((int) (Math.random() * 1000));
+		}
+		System.out.println("Méthode 2 :");
+		for(int i : set)
+			System.out.println(i);
 	}
-	
-	
+
+
 
 }
